7e727c2acde980f183112dbc868625dcf28f3352
Mugi Fadilah <32443765+mugi789@users.noreply.github.com>
2021-11-05 19:11:22 +0700

    penambahan http://

--- subfind.sh
+++ subfind.sh
@@ -5,17 +5,19 @@ printf "\e[93m
 |__   | | | . | . | . |     | .'| |   |  |   __| |   | . | -_|  _|
 |_____|___|___|___|___|_|_|_|__,|_|_|_|  |__|  |_|_|_|___|___|_|  \033[0m
 
-Created by Mugi Fadilah
+		Created by Mugi Fadilah
 "
 echo -ne '\n\r'
-bafer='https://dns.bufferover.run/dns?q=.'
+url='https://dns.bufferover.run/dns?q=.'
 echo -n -e "Masukkan web yg ingin discan (tanpa http/https) : \e[96m"
 read target
 echo -e "\033[0mScanning \033[0;36m$target\033[0m"
 echo -e "\033[0;31m===========\033[0m Hasil \033[0;31m===========\033[0m"
-scan=$bafer$target
-hasil=$(curl -s $scan | jq -r .FDNS_A[] | sed --separate 's/,/\n/g')
-echo -e "$hasil"
+scan=$url$target
+# hasil=$(curl -s $scan | jq -r .FDNS_A[] | sed --separate 's/,/\n/g')
+for hasil in $(curl -s $scan | jq -r .FDNS_A[] | sed --separate 's/,/\n/g'); do
+echo -e http://"$hasil"
+done
 echo -e "\033[0;31m===========\033[0m  ---  \033[0;31m===========\033[0m"
 while true; do
 	read -p "Mau disimpan dalam bentuk file? y/n : " yn
