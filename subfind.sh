a090f2276a03a3ffa4a2198b376866d19f2e8076
Mugi Fadilah <32443765+mugi789@users.noreply.github.com>
2021-11-05 19:45:20 +0700

    Update subfind.sh

--- subfind.sh
+++ subfind.sh
@@ -5,24 +5,23 @@ printf "\e[93m
 |__   | | | . | . | . |     | .'| |   |  |   __| |   | . | -_|  _|
 |_____|___|___|___|___|_|_|_|__,|_|_|_|  |__|  |_|_|_|___|___|_|  \033[0m
 
-		Created by Mugi Fadilah
+	Created by Mugi Fadilah
 "
 echo -ne '\n\r'
 url='https://dns.bufferover.run/dns?q=.'
-echo -n -e "Masukkan web yg ingin discan (tanpa http/https) : \e[96m"
+echo -n -e " Masukkan web yg ingin discan (tanpa http/https) : \e[96m"
 read target
-echo -e "\033[0mScanning \033[0;36m$target\033[0m"
+echo -e "\033[0m Scanning \033[0;36m$target\033[0m"
 echo -e "\033[0;31m===========\033[0m Hasil \033[0;31m===========\033[0m"
 scan=$url$target
-# hasil=$(curl -s $scan | jq -r .FDNS_A[] | sed --separate 's/,/\n/g')
 for hasil in $(curl -s $scan | jq -r .FDNS_A[] | sed --separate 's/,/\n/g'); do
-echo -e http://"$hasil"
+printf '%s\n' http://"$hasil"/
 done
 echo -e "\033[0;31m===========\033[0m  ---  \033[0;31m===========\033[0m"
 while true; do
 	read -p "Mau disimpan dalam bentuk file? y/n : " yn
 	case $yn in
-		[Yy]* ) printf '%s\n' $hasil >> $target.txt; break;;
+		[Yy]* ) printf '%s\n' http://$hasil/ >> $target.txt; echo -e " OK! File tersimpan "$target".txt"; break;;
 		[Nn]* ) exit;;
 		* ) echo -e "Hanya ketik \e[92mY\033[0m atau \e[92mN\033[0m";;
 		esac
