8b2c3d7e5c722f8128aa1e2dd1d43773ab2f3ac1
Mugi Fadilah <32443765+mugi789@users.noreply.github.com>
2021-11-05 16:07:07 +0700

    Subdo Finder no encrypt

---
+++ subdo.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+printf "\e[93m                                                                
+ _____     _     _               _        _____ _       _         
+|   __|_ _| |_ _| |___ _____ ___|_|___   |   __|_|___ _| |___ ___ 
+|__   | | | . | . | . |     | .'| |   |  |   __| |   | . | -_|  _|
+|_____|___|___|___|___|_|_|_|__,|_|_|_|  |__|  |_|_|_|___|___|_|  \033[0m
+
+Created by Mugi Fadilah
+"
+echo -ne '\n\r'
+bafer='https://dns.bufferover.run/dns?q=.'
+echo -n -e "Masukkan web yg ingin discan (tanpa http/https) : \e[96m"
+read target
+echo -e "\033[0mScanning \033[0;36m$target\033[0m"
+echo -e "\033[0;31m===========\033[0m Hasil \033[0;31m===========\033[0m"
+scan=$bafer$target
+hasil=$(curl -s $scan | jq -r .FDNS_A[] | sed --separate 's/,/\n/g')
+echo -e "$hasil"
+echo -e "\033[0;31m===========\033[0m  ---  \033[0;31m===========\033[0m"
+while true; do
+	read -p "Mau disimpan dalam bentuk file? y/n : " yn
+	case $yn in
+		[Yy]* ) printf '%s\n' $hasil >> $target.txt; break;;
+		[Nn]* ) exit;;
+		* ) echo -e "Hanya ketik \e[92mY\033[0m atau \e[92mN\033[0m";;
+		esac
+done
