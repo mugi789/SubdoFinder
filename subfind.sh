61936fa0dccfb4f3e7bc7344176f6017cd125a86
Mugi Fadilah <32443765+mugi789@users.noreply.github.com>
2021-11-06 11:22:41 +0700

    fix save to file

--- subfind.sh
+++ subfind.sh
@@ -21,7 +21,9 @@ echo -e "\033[0;31m===========\033[0m  ---  \033[0;31m===========\033[0m"
 while true; do
 	read -p "Mau disimpan dalam bentuk file? y/n : " yn
 	case $yn in
-		[Yy]* ) printf '%s\n' http://$hasil/ >> $target.txt; echo -e " OK! File tersimpan "$target".txt"; break;;
+		[Yy]* ) for hasil in $(curl -s $scan | jq -r .FDNS_A[] | sed --separate 's/,/\n/g'); do
+		printf '%s\n' http://"$hasil"/
+		done >> $target.txt; echo -e " OK! File tersimpan \e[96m"$target".txt\e[0m"; break;;
 		[Nn]* ) exit;;
 		* ) echo -e "Hanya ketik \e[92mY\033[0m atau \e[92mN\033[0m";;
 		esac
